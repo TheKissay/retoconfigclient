51613e9e92c6e5cb10e40d37cea9a5c616eda976
TheKissay <thekissay112@gmail.com>
2021-10-22 15:20:47 +0200

    Arreglado error con la variable

--- src/main/java/com/example/retoconfigclient/controller/VariableController.java
+++ src/main/java/com/example/retoconfigclient/controller/VariableController.java
@@ -1,18 +1,23 @@
 package com.example.retoconfigclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class VariableController {
 
 	@Value("${variable1.valor}")
 	private String miVariable;
 	
+	@Value("${variable2.valor}")
+	private String miVariable2;
+	
 	@GetMapping(path="variable")
 	public String miVariable() {
-		return this.miVariable;
+		return this.miVariable + " " + miVariable2;
 	}
 
 	
